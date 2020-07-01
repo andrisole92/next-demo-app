8524ea3f214464453f353a5ad54a76f24223312e
Andrey Ruzhentsev <andrisole92@interstruct.com>
2020-06-30 20:27:17 -0500

    Adding offline

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,19 @@
 const Header = () => (
   <div className="header">
     <nav>
-
+      <ul>
+        <li>Sign In</li>
+      </ul>
     </nav>
+    <style jsx>{`
+      *,
+      *::before,
+      *::after {
+        box-sizing: border-box;
+      }
+    `}</style>
   </div>
+
 );
 
 export default Header;
